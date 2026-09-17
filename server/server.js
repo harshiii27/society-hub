93fbc214a27044c231b68d73f4cd3d176618acc4
harshiii <harshitakashyap949@gmail.com>
2026-09-18 02:40:16 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
@@ -49,10 +51,11 @@ function requireAdmin(req, res, next) {
 }
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "harshita@2007",
-  database: "society_hub",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
