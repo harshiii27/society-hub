48a42596973985b19cb575d3b196a3a1050b558e
harshiii <harshitakashyap949@gmail.com>
2026-09-18 11:00:38 +0530

    Use environment port for deployment

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const JWT_SECRET = "society-hub-secret-key";
 //Role-Based Access Control.
